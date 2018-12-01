085ea9c3d86ce78b3fe3928182a0ce90dc57fbcb
Denys Meloshyn <denys.meloshyn@gmail.com>
2018-12-01 19:52:38 +0100

    Trying to inject http loader

--- SharedCode/src/androidMain/kotlin/ApplicationDispatcher.kt
+++
@@ -1,6 +0,0 @@
-package org.kotlin.mpp.mobile
-
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-
-internal actual val ApplicationDispatcher: CoroutineDispatcher = Dispatchers.Default
--- SharedCode/src/commonMain/kotlin/common.kt
+++ SharedCode/src/commonMain/kotlin/common.kt
@@ -1,35 +1,17 @@
 package org.kotlin.mpp.mobile
 
-import io.ktor.client.HttpClient
-import io.ktor.client.request.get
-import io.ktor.client.request.url
-import io.ktor.http.Url
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-
 expect fun platformName(): String
 
 fun createApplicationScreenMessage() : String {
     return "Kotlin Rocks on ${platformName()}"
 }
 
-internal expect val ApplicationDispatcher: CoroutineDispatcher
+interface LoaderI {
+    fun get(url: String, completion: (String) -> Unit)
+}
 
-class ApplicationApi {
-    private val client = HttpClient()
-
-    private var address = Url("https://tools.ietf.org/rfc/rfc1866.txt")
-
-    fun about(callback: (String) -> Unit) {
-        GlobalScope.apply {
-            launch(ApplicationDispatcher) {
-                val result: String = client.get {
-                    url(this@ApplicationApi.address.toString())
-                }
-
-                callback(result)
-            }
-        }
+class Manager(private val loader: LoaderI) {
+    fun loadData(completion: (String) -> Unit) {
+        loader.get("https://api.github.com/users/defunkt", completion)
     }
 }
