9fd95420ff2822dd511a35af0d460d5c2213b934
Denys Meloshyn <denys.meloshyn@visma.com>
2018-12-14 10:57:25 +0100

    trying to add accounting year

--- SharedCode/src/commonMain/kotlin/common.kt
+++ SharedCode/src/commonMain/kotlin/common.kt
@@ -2,16 +2,28 @@ package org.kotlin.mpp.mobile
 
 expect fun platformName(): String
 
-fun createApplicationScreenMessage() : String {
+fun createApplicationScreenMessage(): String {
     return "Kotlin Rocks on ${platformName()}"
 }
 
 interface LoaderI {
     fun get(url: String, completion: (String) -> Unit)
+    fun get(url: String, headers: HashMap<String, String>, completion: (String) -> Unit)
 }
 
 interface IJson {
     fun serialize(data: String): HashMap<String, Any>
+}
+
+class AccountingYear {
+    var fromDate: String? = null
+    var toDate: String? = null
+    var year: Int? = null
+    var periods: String? = null
+    var entries: String? = null
+    var totals: String? = null
+    var vouchers: String? = null
+    var selfLink: String? = null
 }
 
 class Git(val login: String)
@@ -23,6 +35,17 @@ class Manager(private val loader: LoaderI, private val json: IJson) {
             val git = Git(login = data["login"] as String)
 
             completion(git)
+        }
+    }
+
+    fun loadAccountingYears(completion: (List<AccountingYear>) -> Unit) {
+        loader.get("https://restapi.e-conomic.com/accounting-years") { response ->
+            val json = json.serialize(response)
+            val collection = json["collection"] as? List<HashMap<String, String>>
+            val r = collection?.map {
+                val accountingYear = AccountingYear()
+                accountingYear.fromDate = it["fromDate"]
+            }
         }
     }
 }
