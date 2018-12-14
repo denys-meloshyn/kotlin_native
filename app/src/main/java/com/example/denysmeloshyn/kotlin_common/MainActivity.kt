8a48ec5f53ece7b2fa4cefa84f6e4abee9eee07f
Denys Meloshyn <denys.meloshyn@visma.com>
2018-12-14 08:26:15 +0100

    Add to android project

--- app/src/main/java/com/example/denysmeloshyn/kotlin_common/MainActivity.kt
+++ app/src/main/java/com/example/denysmeloshyn/kotlin_common/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.denysmeloshyn.kotlin_common
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.kotlin.mpp.mobile.createApplicationScreenMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        createApplicationScreenMessage()
+
         setContentView(R.layout.activity_main)
     }
 }
